3d05bd3230d415bfc1cf00f89286dc9e5482cf26
Igor Mendes <igormmsouza@gmail.com>
2021-04-09 20:40:49 -0300

    update CalculadoraTest
    implementa os asserts e adiciona o teste de subtração.

--- src/test/java/CalculadoraTest.java
+++ src/test/java/CalculadoraTest.java
@@ -1,15 +1,18 @@
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.concurrent.TimeUnit;
+import org.junit.jupiter.api.AfterAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CalculadoraTest {
 
     private AndroidDriver driver;
+    private int resultado;
 
     @Before
     public void setUp() throws MalformedURLException {
@@ -25,7 +28,9 @@ public class CalculadoraTest {
     }
 
     @Test
-    public void somaTest() {
+    public void verificaSoma2Mais6Test() throws InterruptedException {
+        /* Espera o app carregar*/
+        TimeUnit.SECONDS.sleep(5);
         MobileElement el1 = (MobileElement) driver.findElementById("com.google.android.calculator:id/digit_2");
         el1.click();
         MobileElement el2 = (MobileElement) driver.findElementByAccessibilityId("mais");
@@ -35,10 +40,29 @@ public class CalculadoraTest {
         MobileElement el4 = (MobileElement) driver.findElementByAccessibilityId("igual");
         el4.click();
         MobileElement el5 = (MobileElement) driver.findElementById("com.google.android.calculator:id/result_final");
-        el5.click();
+
+        this.resultado = Integer.parseInt(el5.getText());
+        assertEquals(8, resultado);
+    }
+    @Test
+    public void verificaSubtrai9Menos5Test() throws InterruptedException {
+        /* Espera o app carregar*/
+        TimeUnit.SECONDS.sleep(5);
+        MobileElement el1 = (MobileElement) driver.findElementById("com.google.android.calculator:id/digit_9");
+        el1.click();
+        MobileElement el2 = (MobileElement) driver.findElementByAccessibilityId("menos");
+        el2.click();
+        MobileElement el3 = (MobileElement) driver.findElementById("com.google.android.calculator:id/digit_5");
+        el3.click();
+        MobileElement el4 = (MobileElement) driver.findElementByAccessibilityId("igual");
+        el4.click();
+        MobileElement el5 = (MobileElement) driver.findElementById("com.google.android.calculator:id/result_final");
+
+        this.resultado = Integer.parseInt(el5.getText());
+        assertEquals(4, resultado);
     }
 
-    @After
+    @AfterAll
     public void tearDown() {
         driver.quit();
     }
