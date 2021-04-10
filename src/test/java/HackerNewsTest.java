d2d7f79b47d805866dfffd4caf7d0ed670c2403b
DsMenezes <dsmennezes@gmail.com>
2021-04-10 13:20:37 -0300

    Update HackerNewsTest.java
    inclui asserts e melhorias no teste

--- src/test/java/HackerNewsTest.java
+++ src/test/java/HackerNewsTest.java
@@ -1,16 +1,21 @@
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
-import junit.framework.TestCase;
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.concurrent.TimeUnit;
+import org.junit.jupiter.api.AfterAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class HackerNewsTest {
 
     private AndroidDriver driver;
+    private String msgResultadoLogin;
+    private String msgDeErro = "Arrr…wrong username/password";
+    private String msgLoginVazio = "Catch you, anonymous!";
 
     @Before
     public void setUp() throws MalformedURLException {
@@ -26,7 +31,7 @@ public class HackerNewsTest {
     }
 
     @Test
-    public void sampleTest() {
+    public void testLoginUsuarioInvalido() throws InterruptedException {
         MobileElement el1 = (MobileElement) driver.findElementByAccessibilityId("Navegar para cima");
         el1.click();
         MobileElement el2 = (MobileElement) driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.support.v4.widget.DrawerLayout/android.widget.RelativeLayout/android.view.ViewGroup/android.widget.FrameLayout/android.widget.RelativeLayout/android.view.ViewGroup/android.support.v7.widget.RecyclerView/android.widget.RelativeLayout[1]/android.widget.RelativeLayout/android.widget.TextView[4]");
@@ -39,11 +44,34 @@ public class HackerNewsTest {
         MobileElement el5 = (MobileElement) driver.findElementById("com.leavjenn.hews:id/et_password");
         el5.click();
         el5.sendKeys("132465");
-        MobileElement el6 = (MobileElement) driver.findElementById("android:id/button1");
-        el6.click();
+        MobileElement loginButton = (MobileElement) driver.findElementById("android:id/button1");
+        loginButton.click();
+        //Aguarda tempo de login
+        TimeUnit.SECONDS.sleep(5);
+        MobileElement el7 = (MobileElement) driver.findElementById("com.leavjenn.hews:id/tv_prompt");
+        this.msgResultadoLogin = el7.getText();
+        assertEquals(msgDeErro, msgResultadoLogin);
+
+    }
+    @Test
+    public void testLoginUsuarioVazio() throws InterruptedException {
+        MobileElement el1 = (MobileElement) driver.findElementByAccessibilityId("Navegar para cima");
+        el1.click();
+        MobileElement el2 = (MobileElement) driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.support.v4.widget.DrawerLayout/android.widget.RelativeLayout/android.view.ViewGroup/android.widget.FrameLayout/android.widget.RelativeLayout/android.view.ViewGroup/android.support.v7.widget.RecyclerView/android.widget.RelativeLayout[1]/android.widget.RelativeLayout/android.widget.TextView[4]");
+        el2.click();
+        MobileElement el3 = (MobileElement) driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.support.v4.widget.DrawerLayout/android.widget.RelativeLayout/android.view.ViewGroup/android.widget.FrameLayout/android.widget.RelativeLayout/android.view.ViewGroup/android.support.v7.widget.RecyclerView/android.widget.RelativeLayout[2]/android.widget.RelativeLayout/android.widget.TextView[2]");
+        el3.click();
+        MobileElement loginButton = (MobileElement) driver.findElementById("android:id/button1");
+        loginButton.click();
+        //Aguarda tempo de login
+        TimeUnit.SECONDS.sleep(5);
+        MobileElement el7 = (MobileElement) driver.findElementById("com.leavjenn.hews:id/tv_prompt");
+        this.msgResultadoLogin = el7.getText();
+        assertEquals(msgLoginVazio, msgResultadoLogin);
+
     }
 
-    @After
+    @AfterAll
     public void tearDown() {
         driver.quit();
     }
